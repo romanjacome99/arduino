6ce0bf594bbbe95e544e45eccac67e9fd5b8aac2
Román Alejandro Jácome Carrascal <roaljaca@gmail.com>
2016-05-04 20:22:54 -0500

    negro avanza blanco detenido

--- SeguidorDeLinea/SeguidorDeLinea.ino
+++ SeguidorDeLinea/SeguidorDeLinea.ino
@@ -2,34 +2,40 @@
 int MotorIzquierdo1=6;
 int MotorIzquierdo2=5;
 int VelocidadIzquierdo=127;
-int aceleradoIzquierdo=255;
+int aceleradoIzquierdo=220;
 int MotorDerecho1=11;
 int MotorDerecho2=10;
 int VelocidadDerecho=105;
-int aceleradoDerecho=255;
+int aceleradoDerecho=215;
 void setup() {
   pinMode(MotorIzquierdo1, OUTPUT);
   pinMode(MotorIzquierdo2, OUTPUT);
   pinMode(MotorDerecho1, OUTPUT);
   pinMode(MotorDerecho2, OUTPUT); 
+  pinMode(13, OUTPUT);
   
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-   
- int sensorIzquierdo = analogRead(0);
+  digitalWrite (13,HIGH);
+
+  
+ int sensorIzquierdo = analogRead(2);
  int sensorDerecho = analogRead(1);
 
- if (sensorIzquierdo > 750) motorIzquierdoAdelante (VelocidadIzquierdo);
- else motorIzquierdoAdelante (aceleradoIzquierdo);
- if (sensorDerecho > 750) motorDerechoAdelante (VelocidadDerecho);
- else motorDerechoAdelante (aceleradoDerecho);
+ if (sensorIzquierdo > 800) motorIzquierdoAdelante  (aceleradoIzquierdo);
+ 
+ else motorDerechoDetenido() ;
+ 
+ if (sensorDerecho > 800)   motorDerechoAdelante (aceleradoDerecho);
+ 
+ else motorDerechoDetenido();
  
   
- 
-}
+  
+ }
 
 
 void motorIzquierdoAdelante(int velocidad){
